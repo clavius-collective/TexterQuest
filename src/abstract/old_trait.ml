66e2c2cbce1d73624c99acc23fed58a4c56aa409
David Donna <davidadonna@gmail.com>
2013-01-08 17:53:44 -0500

    added trait module

---
+++ src/abstract/old_trait.ml
@@ -0,0 +1,74 @@
+(* Copyright (C) 2013 Ben Lewis and David Donna *)
+(* Licensed under LGPLv3 *)
+
+(* Types for traits *)
+(* Enumerated types for Attributes, Skills, and Affinities *)
+type attribute = Balance 
+                 | Hardiness
+                 | Might
+                 | Mettle
+                 | Finesse
+                 | Agility
+                 | Stability
+                 | Resilience
+                 | Dedication
+                 | Concentration
+                 | Intuition
+                 | Clarity
+
+type skill = Alchemy
+             | Leatherworking
+
+(* We need to work out a list of skills; unsure what all will go in it.  Those
+ * can probably wait for a while.
+ *)
+
+      
+
+type affinity = Solar
+                | Lunar
+                | Astral
+                | Terra
+                | Aqua
+                | Aero
+                | Bio
+                | Frost
+                | Ego
+                | Ethos
+                | Shadow
+                | Light
+                | Chroma
+                | Flux
+                | Static
+                | Chaos
+                | Sync
+                | Life
+                | Death
+
+(* Variant type to work with all three types of traits. *)
+type trait = Attribute of attribute
+             | Skill of skill
+             | Affinity of affinity
+
+let current_value actor trait =
+  (* Get the current value (with "experience") from the actor, via cases. *)
+  (* Take the floor of the current value, and return it. *)
+ end
+
+let current_step actor trait =
+  (* Get the value of the trait from the actor; this can be handled in three
+  cases, to deal with the different hashmaps involved. *)
+  (* Determine the step involved by first getting the current value, then
+  applying the formula we determine to find the step size. *)
+ end
+
+let add_experience actor trait quantity =
+  (* Get the value of the trait out of the correct hashmap. *)
+  (* Find the current step size *)
+  (* Compute if there is an overflow to the next level.
+   * - If so, add as much as necessary to overflow, then recompute step size and
+   *   repeat the procedure for the new level.
+   * - If not, add to current value.
+   * Write the new value to the hashmap for the same trait.
+   *)
+ end
--- src/abstract/trait.ml
+++ src/abstract/trait.ml
@@ -1,74 +1,58 @@
-(* Copyright (C) 2013 Ben Lewis and David Donna *)
-(* Licensed under LGPLv3 *)
+type aspect = []
+type attribute = []
+type skill = []
+type trait = [ aspect | attribute | skill ]
 
-(* Types for traits *)
-(* Enumerated types for Attributes, Skills, and Affinities *)
-type attribute = Balance 
-                 | Hardiness
-                 | Might
-                 | Mettle
-                 | Finesse
-                 | Agility
-                 | Stability
-                 | Resilience
-                 | Dedication
-                 | Concentration
-                 | Intuition
-                 | Clarity
+let all_aspects = []
+let all_attribute = []
+let all_skills = []
+let all_traits = all_aspects @ all_attributes @ all_traits
 
-type skill = Alchemy
-             | Leatherworking
+let get_time () : int = truncate (Unix.time ())
 
-(* We need to work out a list of skills; unsure what all will go in it.  Those
- * can probably wait for a while.
- *)
+type stat = {
+  mutable value : float;
+  mutable masks : ((int -> int) * int) list
+}
 
-      
+let new_stat ?(value = 0.0) ?(masks = []) () = { value; masks; }
 
-type affinity = Solar
-                | Lunar
-                | Astral
-                | Terra
-                | Aqua
-                | Aero
-                | Bio
-                | Frost
-                | Ego
-                | Ethos
-                | Shadow
-                | Light
-                | Chroma
-                | Flux
-                | Static
-                | Chaos
-                | Sync
-                | Life
-                | Death
+type vector = (trait, stat) Hashtbl.t
 
-(* Variant type to work with all three types of traits. *)
-type trait = Attribute of attribute
-             | Skill of skill
-             | Affinity of affinity
+let lookup vec = Hashtbl.find vec
 
-let current_value actor trait =
-  (* Get the current value (with "experience") from the actor, via cases. *)
-  (* Take the floor of the current value, and return it. *)
- end
+let mask vec trait func duration =
+  let stat = (lookup vec trait) in
+  let expire = duration + get_time () in
+  stat.masks <- (func, expire)::stat.masks
 
-let current_step actor trait =
-  (* Get the value of the trait from the actor; this can be handled in three
-  cases, to deal with the different hashmaps involved. *)
-  (* Determine the step involved by first getting the current value, then
-  applying the formula we determine to find the step size. *)
- end
+let value vec trait =
+  let stat = lookup vec trait in
+  let time = get_time () in
+  let final_val, masks =
+    List.fold_right
+      (fun ((func, expire) as mask) (total, active) ->
+        if expire > time then
+          func(total), mask::active
+        else
+          total, active)
+      stat.masks
+      (truncate stat.value, [])
+  in
+  stat.masks <- masks;
+  final_val
 
-let add_experience actor trait quantity =
-  (* Get the value of the trait out of the correct hashmap. *)
-  (* Find the current step size *)
-  (* Compute if there is an overflow to the next level.
-   * - If so, add as much as necessary to overflow, then recompute step size and
-   *   repeat the procedure for the new level.
-   * - If not, add to current value.
-   * Write the new value to the hashmap for the same trait.
-   *)
- end
+let raw_value vec trait = truncate (lookup vec trait).value
+
+let create ?(initial = []) traits =
+  let table = Hashtbl.create 100 in
+  List.iter
+    (fun k -> Hashtbl.add table k (new_stat ()))
+    traits;
+  table
+
+let check vec coeffs =
+  List.fold_left
+    (fun total (trait, coeff) -> total +. (float (lookup vec trait) *. coeff))
+    0.0
+    coeffs
