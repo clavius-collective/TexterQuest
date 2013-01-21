d8ab2d5eaee18d9d2e3e552cf350b99a0f6a6e96
David Donna <davidadonna@gmail.com>
2013-01-20 20:55:35 -0500

    added a MASKED module type for mask.mli

--- src/abstract/mask.ml
+++ src/abstract/mask.ml
@@ -4,6 +4,14 @@
 
 include Util
 
+module type MASKED = sig
+  type t
+  type acc
+  type mask
+  val add_mask  : t -> mask * int -> unit
+  val get_value : t -> acc
+end
+
 module WithReplace = functor (M : sig
   type t
   type acc
@@ -11,24 +19,26 @@ module WithReplace = functor (M : sig
   val get_base : t -> acc
   val get_masks : t -> (mask * int) list
   val set_masks : t ->  (mask * int) list -> unit
-  val replace : (mask * int) -> (mask * int) option
   val apply_mask : acc -> mask -> acc
+  val replace : (mask * int) -> (mask * int) option
 end) -> struct
-  let add_mask t (func, duration) =
+  include M
+
+  let add_mask t (mask, duration) =
     let expire = duration + get_time () in
-    M.set_masks t ((func, expire)::(M.get_masks t))
+    M.set_masks t ((mask, expire)::(M.get_masks t))
 
   let get_value t =
     let time = get_time () in
     let final_val, active =
       List.fold_right
-        (fun ((func, expire) as mask) (total, active) ->
+        (fun ((mask, expire) as timed) (total, active) ->
           if expire > time then
-            M.apply_mask total func, mask::active
+            M.apply_mask total mask, timed::active
           else
-            match M.replace mask with
-              | Some ((func, _) as mask) ->
-                  M.apply_mask total func, mask::active
+            match M.replace timed with
+              | Some ((mask, _) as timed) ->
+                  M.apply_mask total mask, timed::active
               | None -> total, active)
         (M.get_masks t)
         (M.get_base t, [])
--- src/abstract/mask.mli
+++ src/abstract/mask.mli
@@ -4,17 +4,36 @@
 
 open Util
 
+module type MASKED = sig
+  type t                                (* the basic object *)
+  type acc                              (* the type of the "value" *)
+  type mask                             (* the type of the temporary mask *)
+
+  (* Statefully adds the mask given, with its duration. *)
+  val add_mask  : t -> mask * int -> unit
+
+  (* Returns the "current value", factoring in all active masks. *)
+  val get_value : t -> acc
+end
+
+(*
+ * Provides masking functionality for a module, where the mask type is simply
+ * a relation on the accumulator type.
+ *)
 module StandardMask : functor (M : sig
   type t
   type acc
   val get_base : t -> acc
   val get_masks : t -> ((acc -> acc) * int) list
   val set_masks : t -> ((acc -> acc) * int) list -> unit
-end) -> (sig
-  val add_mask  : M.t -> (M.acc -> M.acc) * int -> unit
-  val get_value : M.t -> M.acc
-end)
+end) ->
+(MASKED with type t = M.t and type acc = M.acc and type mask = M.acc -> M.acc)
 
+(*
+ * Provides masking functionality with a given mask type, not necessarily a
+ * relation on the accumulator type. Like StandardMask, except requires the
+ * mask type, and an apply_mask function.
+ *)
 module WithMask : functor (M : sig
   type t
   type acc
@@ -23,11 +42,13 @@ module WithMask : functor (M : sig
   val get_masks : t -> (mask * int) list
   val set_masks : t -> (mask * int) list -> unit
   val apply_mask : acc -> mask -> acc
-end) -> (sig
-  val add_mask  : M.t -> M.mask * int -> unit
-  val get_value : M.t -> M.acc
-end)
+end) ->
+(MASKED with type t = M.t and type acc = M.acc and type mask = M.mask)
 
+(* 
+ * All features are provided, including a function that takes an expired mask
+ * and potentially replaces it with another.
+ *)
 module WithReplace : functor (M : sig
   type t
   type acc
@@ -37,7 +58,5 @@ module WithReplace : functor (M : sig
   val set_masks : t -> (mask * int) list -> unit
   val apply_mask : acc -> mask -> acc
   val replace   : (mask * int) -> (mask * int) option
-end) -> (sig
-  val add_mask  : M.t -> M.mask * int -> unit
-  val get_value : M.t -> M.acc
-end)
+end) ->
+(MASKED with type t = M.t and type acc = M.acc and type mask = M.mask)
