d23b9232e6201c9906c058be181970091e0132ff
David Donna <davidadonna@gmail.com>
2013-02-22 22:09:11 -0500

    added test files for abstract2 to git tracking

---
+++ src/abstract2/all_test.ml
@@ -0,0 +1,16 @@
+open OUnit
+
+let test_pass () = assert_equal true true
+let test_fail () = assert_equal true false
+
+let add_tests (modname, lst) =
+  List.map (fun (name, test) -> (modname ^ "_" ^ name) >:: test) lst
+
+let suite = "Test" >::: List.flatten (List.map add_tests [
+  "util", Util_test.tests;
+  "core", Core_test.tests;
+  "mask", Mask_test.tests;
+])
+
+let _ =
+  run_test_tt_main suite
